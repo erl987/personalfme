2e8345f9914a0495abf62007cb49fa0da9dcf243
Ralf Rettig <meteoradix@gmx.net>
2022-10-29 17:59:05 +0200

    Remove most Octave dependencies: fix reference file path

--- UnitTests/reference-data/generate_reference_data.py
+++ UnitTests/reference-data/generate_reference_data.py
@@ -21,7 +21,7 @@ from reference_data import create_test_reference_data
 DATA_FILE_NAME = 'test-signal.txt'
 
 # Data for testing IIR filter with high FC
-reference_file_path = 'data/iir-filter-high-fc.json'
+reference_file_path = 'iir-filter-high-fc.json'
 down_sampling_factor = 3
 up_sampling_factor = 2
 is_fir = False
@@ -30,7 +30,7 @@ b, a = cheby1(8, 0.05, 0.5333)
 create_test_reference_data(down_sampling_factor, up_sampling_factor, a, b, is_fir, DATA_FILE_NAME, reference_file_path)
 
 # Data for testing FIR filter with up-sampling
-reference_file_path = 'data/fir-filter-up-sampling.json'
+reference_file_path = 'fir-filter-up-sampling.json'
 down_sampling_factor = 1
 up_sampling_factor = 3
 is_fir = True
@@ -39,7 +39,7 @@ b = firwin(30, 0.6666)
 create_test_reference_data(down_sampling_factor, up_sampling_factor, a, b, is_fir, DATA_FILE_NAME, reference_file_path)
 
 # Data for testing IIR filter with up-sampling
-reference_file_path = 'data/iir-filter-up-sampling.json'
+reference_file_path = 'iir-filter-up-sampling.json'
 down_sampling_factor = 1
 up_sampling_factor = 3
 is_fir = False
@@ -48,7 +48,7 @@ b = 1
 create_test_reference_data(down_sampling_factor, up_sampling_factor, a, b, is_fir, DATA_FILE_NAME, reference_file_path)
 
 # Data for testing FIR filter with high FC
-reference_file_path = 'data/fir-filter-high-fc.json'
+reference_file_path = 'fir-filter-high-fc.json'
 down_sampling_factor = 1
 up_sampling_factor = 1
 is_fir = True
@@ -57,7 +57,7 @@ b = firwin(20, 0.3)
 create_test_reference_data(down_sampling_factor, up_sampling_factor, a, b, is_fir, DATA_FILE_NAME, reference_file_path)
 
 # Data for testing FIR filter with low FC
-reference_file_path = 'data/fir-filter-low-fc.json'
+reference_file_path = 'fir-filter-low-fc.json'
 down_sampling_factor = 1
 up_sampling_factor = 1
 is_fir = True
@@ -66,7 +66,7 @@ b = firwin(20, 0.1)
 create_test_reference_data(down_sampling_factor, up_sampling_factor, a, b, is_fir, DATA_FILE_NAME, reference_file_path)
 
 # Data for testing IIR filter with low FC
-reference_file_path = 'data/iir-filter-low-fc.json'
+reference_file_path = 'iir-filter-low-fc.json'
 down_sampling_factor = 1
 up_sampling_factor = 1
 is_fir = False
@@ -74,7 +74,7 @@ b, a = cheby2(20, 40, 0.2)
 create_test_reference_data(down_sampling_factor, up_sampling_factor, a, b, is_fir, DATA_FILE_NAME, reference_file_path)
 
 # Data for testing FIR filter with down-sampling
-reference_file_path = 'data/fir-filter-down-sampling.json'
+reference_file_path = 'fir-filter-down-sampling.json'
 down_sampling_factor = 3
 up_sampling_factor = 1
 is_fir = True
@@ -83,7 +83,7 @@ b = firwin(30, 0.333)
 create_test_reference_data(down_sampling_factor, up_sampling_factor, a, b, is_fir, DATA_FILE_NAME, reference_file_path)
 
 # Data for testing IIR filter with down-sampling
-reference_file_path = 'data/iir-filter-down-sampling.json'
+reference_file_path = 'iir-filter-down-sampling.json'
 down_sampling_factor = 3
 up_sampling_factor = 1
 is_fir = False
@@ -91,7 +91,7 @@ b, a = cheby1( 8, 0.05, 0.2666 )
 create_test_reference_data(down_sampling_factor, up_sampling_factor, a, b, is_fir, DATA_FILE_NAME, reference_file_path)
 
 # Data for testing FIR filter with re-sampling
-reference_file_path = 'data/fir-filter-re-sampling.json'
+reference_file_path = 'fir-filter-re-sampling.json'
 down_sampling_factor = 3
 up_sampling_factor = 2
 is_fir = True
@@ -100,7 +100,7 @@ b = firwin(30, 0.666)
 create_test_reference_data(down_sampling_factor, up_sampling_factor, a, b, is_fir, DATA_FILE_NAME, reference_file_path)
 
 # Data for testing IIR filter with re-sampling
-reference_file_path = 'data/iir-filter-re-sampling.json'
+reference_file_path = 'iir-filter-re-sampling.json'
 down_sampling_factor = 3
 up_sampling_factor = 2
 is_fir = False
@@ -108,7 +108,7 @@ b, a = cheby1(8, 0.05, 0.5333)
 create_test_reference_data(down_sampling_factor, up_sampling_factor, a, b, is_fir, DATA_FILE_NAME, reference_file_path)
 
 # Data for filter design test case
-reference_file_path = 'data/filter-design.json'
+reference_file_path = 'filter-design.json'
 down_sampling_factor = 3
 up_sampling_factor = 1  # dummy value
 is_fir = False  # dummy value
