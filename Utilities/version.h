f20f8d8a8d911445c0a99088c97d1e304b5ab4c6
Ralf Rettig <meteoradix@gmx.net>
2022-11-19 11:49:54 +0000

    Update version-dependent artefacts

--- Utilities/version.h
+++ Utilities/version.h
@@ -21,8 +21,8 @@ along with this program.If not, see <http://www.gnu.org/licenses/>
 *	Currently the detection of 5-tone sequences is implemented. The detected sequences are logged and further
 *	actions can automatically be started, when detecting a sequence.
 *	@author		Ralf Rettig (info@personalfme.de, www.personalfme.de)
-*	@version	714e5ef
-*	@date		"2022-11-06 17:33:52 +0100"
+*	@version	623e048
+*	@date		"2022-11-19 11:41:19 +0000"
 *
 *	The object-oriented code is currently designed for 5-tone sequences only, but it can be flexibly extended to other 
 *	types of sequences, because the basic class "CSearch" is an abstract interface.
@@ -52,9 +52,9 @@ namespace Utilities {
 	/**	@param	VERSION_TYPE		storing the version type (i.e. "" = Release to Manufacturing, "RC1" = Release candidate 1, "pre-release" = Development version) */
 	const std::string VERSION_TYPE = "";
 	/**	@param	REVISION			storing the revision number from the current build, automatically updated by SVN */
-	const std::string REVISION = "714e5ef";
+	const std::string REVISION = "623e048";
 	/**	@param	DATE_STRING			storing the build date from the current revision, automatically updated by SVN */
-	const std::string DATE_STRING = "2022-11-06 17:33:52 +0100";
+	const std::string DATE_STRING = "2022-11-19 11:41:19 +0000";
 	/**	@param	SOFTWARE_NAME		storing the name of the software, automatically updated by SVN */
 	const std::string SOFTWARE_NAME_STRING = "PersonalFME";
 	/**	@param	VENDOR_NAME			storing the name of the software owner, automatically updated by SVN */
