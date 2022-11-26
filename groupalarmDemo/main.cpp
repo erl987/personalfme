5eaa3ef0259a7d91cb29cadc5f254578f58e6813
Ralf Rettig <meteoradix@gmx.net>
2022-11-26 18:29:36 +0100

    Data request at groupalarm.com

--- groupalarmDemo/main.cpp
+++ groupalarmDemo/main.cpp
@@ -24,33 +24,92 @@ along with this program.If not, see <http://www.gnu.org/licenses/>
 #endif
 
 #include <iostream>
+#include <fstream>
 #include "Poco/Net/HTTPResponse.h"
 #include "Poco/StreamCopier.h"
 #include "Poco/Net/HTTPRequest.h"
 #include "Poco/Net/HTTPSessionFactory.h"
 #include "Poco/Net/HTTPSClientSession.h"
+#include "Poco/JSON/Parser.h"
 
-int main() {
+
+const std::string uriStr = "https://app.groupalarm.com/api/v1/units";
+
+
+std::pair<std::string, std::string> readConfigFile(const std::string& configFilePath)
+{
+	using namespace std;
+	using namespace Poco::JSON;
+
+	string organizationId, apiToken;
+
+	try {
+		cout << "Reading config file: " << configFilePath << endl;
+
+		ifstream file(configFilePath);
+		string json;
+		if (file) {
+			ostringstream ss;
+			ss << file.rdbuf();
+			json = ss.str();
+		}
+
+		Parser parser;
+		auto result = parser.parse(json);
+		Object::Ptr pObject = result.extract<Object::Ptr>();
+
+		organizationId = pObject->getValue<string>("organization_id");
+		apiToken = pObject->getValue<string>("api_token");
+	}
+	catch (Poco::Exception e) {
+		std::cout << "Error reading config file: " << e.displayText() << std::endl;
+	}
+
+	return pair<string, string>(organizationId, apiToken);
+}
+
+int main(int argc, char** argv)
+{
 	using namespace std;
 	using namespace Poco;
 	using namespace Poco::Net;
+	using namespace Poco::JSON;
 
-	std::string hostname = "personalfme.de";
-	unsigned short port = 443;
+	if (argc != 2) {
+		std::cout << "Required parameter: configuration file path" << std::endl;
+		return -2;
+	}
 
+	auto config = readConfigFile(argv[1]);
+	string organizationId = config.first;
+	string apiToken = config.second;
 
 	try {
-		HTTPSClientSession session(hostname, port);
+		URI uri(uriStr);
+		HTTPSClientSession session(uri.getHost(), uri.getPort());
 
-		HTTPRequest request(HTTPRequest::HTTP_GET, hostname, HTTPMessage::HTTP_1_1);
+		uri.addQueryParameter("organization", organizationId);
+
+		string path(uri.getPathAndQuery());
+		if (path.empty()) path = "/";
+
+		HTTPRequest request(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
+		request.setContentType("application/json");
+		request.add("API-Token", apiToken);
 
 		session.sendRequest(request);
 		HTTPResponse response;
 
-		istream& is = session.receiveResponse(response);
+		istream& stream = session.receiveResponse(response);
 		cout << response.getStatus() << " " << response.getReason() << endl;
-		cout << "Success" << endl;
-		StreamCopier::copyStream(is, cout);
+		std::string json(std::istreambuf_iterator<char>(stream), {});
+
+		Parser parser;
+		auto result = parser.parse(json);
+		Array::Ptr pObject = result.extract<Array::Ptr>();
+
+		auto description = pObject->get(0).extract<Object::Ptr>();
+		cout << description->getValue<std::string>("description") << endl;;
 	}
 	catch (const Poco::Exception& e) {
 		cout << "Error: " << e.displayText() << endl;
